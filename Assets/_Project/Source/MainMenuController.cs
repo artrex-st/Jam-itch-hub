37b67fdfeb32a6af0fe78513329f07f68a829ae5
Arthur St <artrexwow@gmail.com>
2023-11-01 22:19:12 -0300

    Add Sample menu + TMP

---
+++ Assets/_Project/Source/MainMenuController.cs
@@ -0,0 +1,62 @@
+using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using JIH.ScreenService;
+
+namespace Source
+{
+    public sealed class MainMenuController : BaseScreen
+    {
+        [SerializeField] private Button _PlayButton;
+        [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _exitGameButton;
+        [Header("Screen Reference")]
+        [SerializeField] private ScreenReference _gameScreenRef;
+        [SerializeField] private ScreenReference _settingsScreenRef;
+
+        private void OnEnable()
+        {
+            Initialize();
+        }
+
+        private void OnDisable()
+        {
+            Dispose();
+        }
+
+        private new void Initialize()
+        {
+            base.Initialize();
+            _PlayButton.onClick.AddListener(PlayButtonClickHandler);
+            _settingsButton.onClick.AddListener(SettingsButtonClickHandler);
+            _exitGameButton.onClick.AddListener(ExitGameButtonClickHandler);
+
+            SoundService.PlayMusic(SoundService.SoundLibrary.MainMenuMusic);
+        }
+
+        private void PlayButtonClickHandler()
+        {
+            AsyncOperation openSceneOperationAsync = ScreenService.LoadSingleSceneAsync(_gameScreenRef);
+            openSceneOperationAsync.completed += async operation =>
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(3));
+                AsyncOperation backSceneAsync = ScreenService.LoadSingleSceneAsync(_thisScreenRef);
+            };
+        }
+
+        private void SettingsButtonClickHandler()
+        {
+            AsyncOperation openSceneOperationAsync = ScreenService.LoadAdditiveSceneAsync(_settingsScreenRef);
+        }
+
+        private void ExitGameButtonClickHandler()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
---
+++ Assets/_Project/Source/SettingsMenuController.cs
@@ -0,0 +1,88 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+using JIH.ScreenService;
+
+namespace Source
+{
+    public class SettingsMenuController : BaseScreen
+    {
+        [SerializeField] private Button _closeButton;
+        [FoldoutGroup("Settings UiOverlay elements")]
+        [SerializeField] private Slider _sliderMaster;
+        [FoldoutGroup("Settings UiOverlay elements")]
+        [SerializeField] private Slider _sliderMusic;
+        [FoldoutGroup("Settings UiOverlay elements")]
+        [SerializeField] private Slider _sliderSfx;
+        [FoldoutGroup("Settings UiOverlay elements")]
+        [SerializeField] private Slider _sliderUiSfx;
+        [FoldoutGroup("Settings config")]
+        [SerializeField] private float _soundFeedBackThreshold = 1;
+        private float _sliderValue;
+
+        private void OnEnable()
+        {
+            Initialize();
+        }
+
+        private void OnDisable()
+        {
+            Dispose();
+        }
+
+        private new void Initialize()
+        {
+            base.Initialize();
+            _closeButton.onClick.AddListener(CloseButtonClickHandler);
+            _sliderMaster.onValueChanged.AddListener(OnMasterVolumeChangeHandler);
+            _sliderMusic.onValueChanged.AddListener(OnMusicVolumeChangeHandler);
+            _sliderSfx.onValueChanged.AddListener(OnSfxVolumeChangeHandler);
+            _sliderUiSfx.onValueChanged.AddListener(OnUiSfxVolumeChangeHandler);
+
+            SyncSlidersFromMixers();
+        }
+
+        private void OnMasterVolumeChangeHandler(float value)
+        {
+            SoundService.MasterVolume = value;
+        }
+
+        private void OnMusicVolumeChangeHandler(float value)
+        {
+            SoundService.MusicVolume = value;
+        }
+
+        private void OnSfxVolumeChangeHandler(float value)
+        {
+            SoundService.SfxVolume = value;
+        }
+
+        private void OnUiSfxVolumeChangeHandler(float value)
+        {
+            SoundService.UiSfxVolume = value;
+        }
+
+        private void CloseButtonClickHandler()
+        {
+            SaveSoundVolume();
+            AsyncOperation openSceneOperationAsync = ScreenService.UnLoadSceneAsync(_thisScreenRef);
+        }
+
+        private void SyncSlidersFromMixers()
+        {
+            _sliderMaster.value = SaveDataService.GameData.MasterVolume;
+            _sliderMusic.value = SaveDataService.GameData.MusicVolume;
+            _sliderSfx.value = SaveDataService.GameData.SfxVolume;
+            _sliderUiSfx.value = SaveDataService.GameData.UiSfxVolume;
+        }
+
+        private void SaveSoundVolume()
+        {
+            SaveDataService.GameData.MasterVolume = SoundService.MasterVolume;
+            SaveDataService.GameData.MusicVolume = SoundService.MusicVolume;
+            SaveDataService.GameData.SfxVolume = SoundService.SfxVolume;
+            SaveDataService.GameData.UiSfxVolume = SoundService.UiSfxVolume;
+            SaveDataService.SaveGame();
+        }
+    }
+}
